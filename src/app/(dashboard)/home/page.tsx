9ad7d21940c3ea9eec68e3bd49931b8e93e115c1
ShivamSignfeed <shivam@signfeed.in>
2025-12-09 17:35:55 +0530

    first commit

---
+++ src/app/(dashboard)/home/page.tsx
@@ -0,0 +1,36 @@
+import { dividerClasses } from '@mui/material'
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Home page!</h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, doloremque! Recusandae asperiores iusto minus
+        quos, non quam quod quae deserunt quis deleniti. Id excepturi consequuntur tempora alias, molestiae accusamus?
+        Velit at rem unde facilis, id magnam vero molestias facere! Tempore, aliquid quisquam voluptatem adipisci
+        tempora amet veritatis facilis eius, officiis corporis commodi accusantium minus dolore. Voluptatum animi
+        repudiandae quod aspernatur voluptates sunt blanditiis eaque deserunt dicta perspiciatis nam, quia ducimus atque
+        amet mollitia placeat provident neque ipsa error fugit dolores ea at consectetur? Sed officiis, ipsa recusandae
+        nemo dolorum quia doloremque. Molestias soluta quas voluptas autem hic nam. Quisquam, fuga.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, doloremque! Recusandae asperiores iusto minus
+        quos, non quam quod quae deserunt quis deleniti. Id excepturi consequuntur tempora alias, molestiae accusamus?
+        Velit at rem unde facilis, id magnam vero molestias facere! Tempore, aliquid quisquam voluptatem adipisci
+        tempora amet veritatis facilis eius, officiis corporis commodi accusantium minus dolore. Voluptatum animi
+        repudiandae quod aspernatur voluptates sunt blanditiis eaque deserunt dicta perspiciatis nam, quia ducimus atque
+        amet mollitia placeat provident neque ipsa error fugit dolores ea at consectetur? Sed officiis, ipsa recusandae
+        nemo dolorum quia doloremque. Molestias soluta quas voluptas autem hic nam. Quisquam, fuga.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, doloremque! Recusandae asperiores iusto minus
+        quos, non quam quod quae deserunt quis deleniti. Id excepturi consequuntur tempora alias, molestiae accusamus?
+        Velit at rem unde facilis, id magnam vero molestias facere! Tempore, aliquid quisquam voluptatem adipisci
+        tempora amet veritatis facilis eius, officiis corporis commodi accusantium minus dolore. Voluptatum animi
+        repudiandae quod aspernatur voluptates sunt blanditiis eaque deserunt dicta perspiciatis nam, quia ducimus atque
+        amet mollitia placeat provident neque ipsa error fugit dolores ea at consectetur? Sed officiis, ipsa recusandae
+        nemo dolorum quia doloremque. Molestias soluta quas voluptas autem hic nam. Quisquam, fuga.
+      </h1>
+    </div>
+  )
+}
